687c9a39ea1b3d61e4eae20a2ab2f7da59520f6a
松井彩里 <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2020-03-21 18:44:29 +0900

    3種類の食べ物を判定し料理名とカロリーを返せる

---
+++ machine_learning/judge.py
@@ -0,0 +1,48 @@
+# 何の料理かを判定し、カロリーを返す
+import keras
+from keras.models import load_model
+from keras.preprocessing.image import load_img
+import numpy as np
+
+class Judge():
+
+    def __init__(self, img_path):
+        self.model = keras.models.load_model('VGG16/food_recognition.h5')
+        self.img_path = img_path
+        self.food = ['カレーライス', '牛丼', 'ラーメン']
+        self.calories = [750, 656, 540]
+
+    # 学習したモデルに基づいて何の料理なのかを判定
+    def judge(self):
+        img = keras.preprocessing.image.load_img(self.img_path, target_size=(224, 224))
+        x = keras.preprocessing.image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = x / 255.0
+
+        pred = self.model.predict(x)[0]
+
+        category = np.argmax(pred)
+
+        return category
+
+    # 料理名を返す
+    def get_food(self):
+        cat = self.judge()
+        return self.food[cat]
+
+    # カロリーを返す
+    def get_calories(self):
+        cat = self.judge()
+        return self.calories[cat]
+
+
+# テスト用
+'''
+if __name__ == '__main__':
+    img_path = 'test_images/ramen_test.jpg'
+    judge = Judge(img_path)
+    result_food = judge.get_food()
+    result_calories = judge.get_calories()
+    print('この食べ物は、' + result_food)
+    print('カロリーは、' + str(result_calories) + 'kcal')
+'''
